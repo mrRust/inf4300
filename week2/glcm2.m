e258491e95ad0d51174350f0eddec83cc4fc5e1e
mrRust <hansrus@ulrik.uio.no>
2015-09-08 09:50:19 +0200

    "Index start and stop complete "

--- week2/glcm2.m
+++ week2/glcm2.m
@@ -1,15 +1,19 @@
-function p = glcm2(img, dx, dy, g)
-    img1 = double(img);
-    goal = log2(g)    
+function p = glcm2(img, dy, dx, G)
+    [M, N, z] = size(img)
+    img1 = double(img(:,:,1))
+    goal = log2(G)    
     currentlevel = ceil(log2(max(img1(:))))
     shift = goal-currentlevel
     img2 = bitshift(img1, shift);
     %Shifting complete. 
     
     
+    p = zeros(G);
+    start_m = max(1, 1-dy)
+    start_n = max(1, 1-dx)
+    stop_m = min(M, M-dy)
+    stop_n = min(N, N-dx)
+    % Indexing start and stop complete
     
-    
-    
-    
-    
+   
 end
