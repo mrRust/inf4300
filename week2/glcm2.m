ee055d0302d91423a41bef0d88047b0c8955d3e6
mrRust <hansrus@ulrik.uio.no>
2015-09-16 15:41:47 +0200

    Week 2 done. Ok result

--- week2/glcm2.m
+++ week2/glcm2.m
@@ -1,19 +1,30 @@
 function p = glcm2(img, dy, dx, G)
-    [M, N, z] = size(img)
-    img1 = double(img(:,:,1))
-    goal = log2(G)    
-    currentlevel = ceil(log2(max(img1(:))))
-    shift = goal-currentlevel
+    [M, N, z] = size(img);
+    img1 = double(img(:,:,1));
+    goal = log2(G);    
+    currentlevel = ceil(log2(max(img1(:))));
+    shift = floor(goal-currentlevel);
+    
     img2 = bitshift(img1, shift);
     %Shifting complete. 
     
     
     p = zeros(G);
-    start_m = max(1, 1-dy)
-    start_n = max(1, 1-dx)
-    stop_m = min(M, M-dy)
-    stop_n = min(N, N-dx)
-    % Indexing start and stop complete
     
+    start_m = max(1, 1-dy);
+    start_n = max(1, 1-dx);
+    stop_m = min(M, M-dy);
+    stop_n = min(N, N-dx);
+    % Indexing start and stop complete
+    for m = start_m:stop_m
+        for n = start_n:stop_n
+          
+            p(img2(m, m+dy)+1, img2(n, n+dx)+1) =   p(img2(m, m+dy)+1, img2(n, n+dx)+1) +1;
    
+        end
+    end
+ 
+    
+    p = p/((stop_m-start_m+1)*(stop_n-start_n+1));
+
 end
--- week2/zebramain.m
+++ week2/zebramain.m
@@ -1,19 +1,97 @@
+%% GLCM window test
 clear all;
 img= imread('zebra_1.tif');
-glcm2(img(1:10, 1:10), 1, 0, 8)
+img1 = img(190:200, 190:200)
+img2 = glcm2(img1, 2, 0, 8);
 
-%{
+%% GLCM Features test
+[M N] = size(img2);
+
+img_v = 0; %Variace
+img_c = 0; %Contrast
+img_e = 0; %Entropy
+avg = 1/(M*N);
+for m = 1:M
+    for n = 1:N        
+        img_v = img_v + (avg-img2(m,n))^2;
+        if (m ~= n)
+            img_c = img_c + (img2(m, n)*(m-n)^2);
+        end
+        if(img2(m,n) ~= 0)
+            img_e = img_e -(img2(m, n)*log(img2(m,n)))
+        end
+    end
+end
+
+img_v = img_v/(M*N)
+img_c = img_c/(M*N)
+img_e = img_e/(M*N)
+%% Computing GLCM features sliding window
+ws = 7;
+cutoff = 2;
+dx = 2;
+dy = 2;
+levels = 8;
+glcm_img = zeros(ws);
+img = imread('zebra_1.tif');
+img2 = double(img);
+[M N] = size(img2);
+var_img = zeros(M,N);
+con_img = zeros(M,N);
+ent_img = zeros(M,N);
+
+for m=1+cutoff:M-cutoff-ws
+    for n=1+cutoff:N-cutoff-ws
+        glcm_img = glcm2(img2(m:m+ws, n:n+ws), dx, dy, ws);
+        % GLCM features
+        img_v = 0;
+        img_c = 0;
+        img_e = 0;
+        for k = 1:ws
+            for j = 1:ws        
+                img_v = img_v + (avg-glcm_img(k,j))^2;
+                if (k ~= j)
+                    img_c = img_c + (glcm_img(k, j)*(k-j)^2);
+                end
+                if(glcm_img(k,j) ~= 0)
+                    img_e = img_e -(glcm_img(k, j)*log(glcm_img(k,j)));
+                end
+            end
+        end
+        var_img(m,n) = img_v;
+        ent_img(m,n) = img_e;
+        con_img(m,n) = img_c;
+        
+    end
+end
 %%
+img3 = img2/256;
 
-First, try to implement your own GLCM function that takes as input an image
-window and number of image greyscales and outputs a co-occurence matrix.
-Derive variance, contrast and entropy from the GLCM of a sliding window at a
-suitable size.
-Try to use a simple tresholding of these features to mask out the zebras in the
-images.
-Then compare your result with the first order texture measures: variance and
-entropy by using the Matlab functions: stdfilt and entropyfilt.
-If you have time, try to use Laws texture masks to analyze the image with a
-suitable mask or two. Remember to average energy over windows. Laws masks
-can be built and applied with conv2 function in Matlab. 
-%}
+figure(1)
+subplot(221)
+imshow(img3)
+subplot(222)
+imshow(var_img)
+subplot(223)
+imshow(ent_img)
+subplot(224)
+imshow(con_img)
+avg_var = mean(var_img(:))
+avg_con = mean(con_img(:))
+avg_ent = mean(ent_img(:))
+figure(2)
+
+
+img4 = ((var_img < 0.5)+0);
+img5 = ((con_img > 0.5)+0);
+img6 = ((ent_img > 0.7)+0);
+subplot(222)
+imshow(img4)
+subplot(223)
+imshow(img5)
+subplot(224)
+imshow(img6)
+figure(3)
+[M N] = size(img6)
+
+
